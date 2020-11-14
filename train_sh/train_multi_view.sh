78e5a75c9ea890d42d068edd92656a7557cb4d2d
Amardeep Kumar <kumaramardipsingh@gmail.com>
2020-11-14 18:54:18 +0530

    bug fix of BART path

--- train_sh/train_multi_view.sh
+++ train_sh/train_multi_view.sh
@@ -3,7 +3,7 @@ WARMUP_UPDATES=200
 LR=3e-05
 MAX_TOKENS=800
 UPDATE_FREQ=32
-BART_PATH=PATH-TO-BART-MODEL (./bart.large/model.pt)
+BART_PATH=""
 
 CUDA_VISIBLE_DEVICES=0 python train.py cnn_dm-bin_2 \
     --restore-file $BART_PATH \
